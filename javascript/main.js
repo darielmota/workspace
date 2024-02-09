f96b73ec337a33b8ac5990cce694ec8946a63eb7
Dariel mota <motadariel405@gmail.com>
2024-02-09 17:21:15 -0400

    curso

---
+++ javascript/main.js
@@ -0,0 +1,35 @@
+//intro a javascript
+
+/* 
+var nombre = "dariel";
+var apellido = "mota";
+
+document.querySelector("body").innerHTML = " hola " + nombre + apellido;
+*/ 
+
+// operadores aritmeticos
+/*
+ muliplicacion *
+ division /
+ suma +
+ resta -
+ */
+
+/*
+ var nuemro = 16;
+ var numero2 = 587;
+ var resultado = nuemro / numero2;
+
+ console.log(resultado + 5)
+*/
+
+
+// cadenas
+
+var nombre = "juan";
+
+if( nombre.indexOf("dariel") != -1){
+    console.log(" wellcome to the one piece")
+} else{
+    console.log("fuck you")
+}wfff
