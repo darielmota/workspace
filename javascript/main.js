04fa3e1b38d0b9b729b7e5b8521a773649ddb614
Dariel mota <motadariel405@gmail.com>
2024-02-13 00:44:46 -0400

    a dormir

--- javascript/main.js
+++ javascript/main.js
@@ -26,10 +26,53 @@ document.querySelector("body").innerHTML = " hola " + nombre + apellido;
 
 // cadenas
 
-var nombre = "juan";
+// var nombre = "juan";
 
-if( nombre.indexOf("dariel") != -1){
-    console.log(" wellcome to the one piece")
-} else{
-    console.log("fuck you")
-}wfff
+// if( nombre.indexOf("dariel") != -1){
+//     console.log(" wellcome to the one piece")
+// } else{
+//     console.log("fuck you")
+// }wfff
+
+//condiciones
+
+/* operadores logicos
+and &&
+or ||
+igual ==
+oblicatorio igual ===
+diferente  ! not
+diferente igual !=
+
+*/
+
+// var primero = 30;
+// var segundo = 150;
+// var numero = 500;
+
+// if(primero < segundo && numero > segundo ){
+//     // console .log("si")
+// }
+
+// else if (true) {
+//     // console.log("no mio la maco")
+// };
+
+//siclo while
+
+var contador = 0;
+// while (  contador <= 100) {
+//     contador++
+//     console.log(contador)
+// }
+
+// do{
+//     contador++ 
+//     console.log(contador)
+// }
+    
+//     while(contador < 1000)
+
+for (let i = 0; i < 10; i++){
+    console.log(i)
+}
