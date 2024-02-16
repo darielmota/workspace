20766f12317cf55ce2348deba84559033cb9273a
Dariel mota <motadariel405@gmail.com>
2024-02-16 00:13:34 -0400

    practica

--- javascript/main.js
+++ javascript/main.js
@@ -1,81 +1,54 @@
 //intro a javascript
 
-/* 
-var nombre = "dariel";
-var apellido = "mota";
+var palabra = "dariel";
+var palabra2 = "mota";
+// document.querySelector("body").innerHTML = " Hola Mundo";
 
-document.querySelector("body").innerHTML = " hola " + nombre + apellido;
-*/ 
+// operadores aritmeticos :suma"+", resta"-" multiplicacion"*", divicion"/";
 
-// operadores aritmeticos
-/*
- muliplicacion *
- division /
- suma +
- resta -
- */
-
-/*
  var nuemro = 16;
  var numero2 = 587;
  var resultado = nuemro / numero2;
-
- console.log(resultado + 5)
-*/
+//  console.log(resultado + 5)
 
 
 // cadenas
 
-// var nombre = "juan";
+var nombre = "juan";
+if( nombre.indexOf("dariel") != -1){
+    // console.log(" wellcome to the one piece")
+} else{
+    // console.log("fuck you")
+}
 
-// if( nombre.indexOf("dariel") != -1){
-//     console.log(" wellcome to the one piece")
-// } else{
-//     console.log("fuck you")
-// }wfff
+//operadores logicos:and"&&", not"!", igual"=", igual estricto"===", igual o diferente"!=", igual o diferente estricto"!==";
 
-//condiciones
+var primero = 30;
+var segundo = 150;
+var numero = 500;
 
-/* operadores logicos
-and &&
-or ||
-igual ==
-oblicatorio igual ===
-diferente  ! not
-diferente igual !=
+if(primero < segundo && numero > segundo ){
+    // console .log("si")
+}
 
-*/
+else if (true) {
+    // console.log("no mio la maco")
+};
 
-// var primero = 30;
-// var segundo = 150;
-// var numero = 500;
-
-// if(primero < segundo && numero > segundo ){
-//     // console .log("si")
-// }
-
-// else if (true) {
-//     // console.log("no mio la maco")
-// };
-
-//siclo while
+//ciclo while
 
 var contador = 0;
-// while (  contador <= 100) {
-//     contador++
-//     console.log(contador)
-// }
+while (  contador <= 100) {
+    contador++
+    // console.log(contador)
+}
 
-// do{
-//     contador++ 
-//     console.log(contador)
-// }
-    
-//     while(contador < 1000)
-
-// for (let i = 0; i < 10; i++){
-//     console.log(i)
-// }
+do{
+    contador++ 
+    // console.log(contador)
+} 
+   
+while(contador < 1000)
 
 //typeof null underfile
 
@@ -86,7 +59,50 @@ var nombre = "hola";
 var num = null;
 
 if (typeof hola == "undefined") {
-    console.log("hola que hace")
+//     console.log("hola que hace")
+// console.log(undefined)
 }
 
-console.log(undefined)
+//ciclo for
+
+for (let i = 0; i < 10; i++){
+    // console.log(i)
+}
+
+//practica 1
+
+//numero maximo
+var max = 100;
+
+//numero minimo
+var min = 1;
+
+//numero aleatorio
+var numero_secreto = Math.random() * (max-min) + min;
+numero_secreto = parseInt(numero_secreto);
+//0.....1 esto es de la propiedad math es a los numero o cantidad que llega
+
+console.log(numero_secreto);
+
+var mensaje = "ingresa un numero para adivinar el numero magico";
+
+while(true){
+    var numero_del_usuario = prompt(mensaje, "0");
+
+    numero_del_usuario = parseInt(numero_del_usuario);
+    
+    if(numero_del_usuario === numero_secreto){
+        alert("ganaste");
+        break;
+    }
+    else if(numero_del_usuario === 0){
+        break;
+    }
+    else if(numero_del_usuario > numero_secreto){
+        mensaje = "lo sentimos el numero es mayor a  el numero magico";
+    }
+    else if(numero_del_usuario < numero_secreto){
+        mensaje = "lo sentimos , el nmero es menor al numero magico";
+    }
+    
+}
